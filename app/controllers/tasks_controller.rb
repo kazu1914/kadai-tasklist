3e0f96873691477227b88277a4bf70da15d0a47d
kazuhiro.osugi <kazu1914hiro@outlook.jp>
2021-09-02 11:51:21 +0000

    third commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+   before_action :correct_user, only: [:show]
   
   def index
-    @tasks = current_user.tasks.all
+   @tasks = current_user.tasks.all
   end
   
   def show
@@ -54,11 +55,13 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+    
   
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
-  end  
+  end 
+  
 end
